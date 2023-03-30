573a94eeabf27c57a1a7f94913397c767dc477b9
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-03-30 15:08:11 +0300

    forms and styles

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+import Registration from "./Screens/RegistrationScreen";
+// import Login from "./Screens/LoginScreen";
+import { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fonts = () =>
+  Font.loadAsync({
+    "roboto-regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "roboto-bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "roboto-medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
 
 export default function App() {
+  const [font, setFont] = useState(false);
+
+  if (!font) {
+    return (
+      <AppLoading
+        startAsync={fonts}
+        onFinish={() => setFont(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ImageBackground
+          style={styles.image}
+          source={require("./assets/images/bg.png")}
+        >
+          <Registration />
+          {/* <Login /> */}
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily: "roboto-regular",
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    // alignItems: "center",
+    justifyContent: "flex-end",
   },
 });
